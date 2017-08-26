e388089e43ab51f2444bbbc854911a3f0862bdda
Victor Nitu <nitu.v.r@gmail.com>
2017-08-27 00:05:33 +0200

    Refactor translation system

--- website/index.js
+++ website/index.js
@@ -5,27 +5,26 @@ const write = promisify(writeFile)
 const render = promisify(sass.render)
 const pug = require('pug')
 
-const writeHtml = async (name, html) => {
-    await write(`dist/${name}.html`, html)
-    console.log(`Page ${name} generated`)
+const writeHtml = async (name, language, html) => {
+    await write(`dist/${language}/${name}.html`, html)
+    console.log(`Page ${name} generated in ${language}`)
 }
 const writeCss = async _ => {
     const file = 'src/styles.scss'
     const style = await render({file})
-    await write('dist/styles.css', style.css)
+    await write('dist/styles/main.css', style.css)
     console.log('Style generated')
 }
 
 const languages = ['en', 'fr']
 
-const compiler = input => {
-    const compiledFunction = pug.compileFile(`src/${input}.pug`)
-    return async (language, output = language) => {
+const compiler = name => {
+    const compiledFunction = pug.compileFile(`src/${name}.pug`)
+    return async language => {
         try {
             const i18n = require(`./i18n/${language}.json`)
-            await writeHtml(output, compiledFunction({languages, i18n}))
-        }
-        catch (err) {
+            await writeHtml(name, language, compiledFunction({languages, i18n}))
+        } catch (err) {
             console.error(err)
         }
     }
@@ -35,8 +34,7 @@ const main = async _ => {
     const compileIndex = compiler('index')
     try {
         await writeCss()
-        await compileIndex(languages[0], 'index')
-        await languages.forEach(l => compileIndex(l))
+        await languages.forEach(compileIndex)
     } catch (err) {
         console.error(err)
     }
