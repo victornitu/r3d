0663655025c1f81da388bd8996723ce0e7e0fa41
Unknown <tilmansroger@gmail.com>
2017-09-05 17:04:08 -0300

    make google maps work and minor changes in contact section

---
+++ website/dist/js/maps.js
@@ -0,0 +1,98 @@
+$(function () {
+  // Google maps
+  var mapLocation = new google.maps.LatLng(32.689593,-117.18039);
+  var map = new GMaps({
+  streetViewControl : false,
+  overviewMapControl: false,
+  mapTypeControl: false,
+  panControl : false,
+  scrollwheel: false,
+  center: mapLocation,
+  el: '#map',
+  zoom: 15,
+  styles: [
+  {
+  "featureType": "administrative",
+  "elementType": "labels.text.fill",
+  "stylers": [
+  {
+  "color": "#444444"
+  }
+  ]
+  },
+  {
+  "featureType": "landscape",
+  "elementType": "all",
+  "stylers": [
+  {
+  "color": "#f2f2f2"
+  }
+  ]
+  },
+  {
+  "featureType": "poi",
+  "elementType": "all",
+  "stylers": [
+  {
+  "visibility": "off"
+  }
+  ]
+  },
+  {
+  "featureType": "road",
+  "elementType": "all",
+  "stylers": [
+  {
+  "saturation": -100
+  },
+  {
+  "lightness": 45
+  }
+  ]
+  },
+  {
+  "featureType": "road.highway",
+  "elementType": "all",
+  "stylers": [
+  {
+  "visibility": "simplified"
+  }
+  ]
+  },
+  {
+  "featureType": "road.arterial",
+  "elementType": "labels.icon",
+  "stylers": [
+  {
+  "visibility": "off"
+  }
+  ]
+  },
+  {
+  "featureType": "transit",
+  "elementType": "all",
+  "stylers": [
+  {
+  "visibility": "off"
+  }
+  ]
+  },
+  {
+  "featureType": "water",
+  "elementType": "all",
+  "stylers": [
+  {
+  "color": "#c0e4f3"
+  },
+  {
+  "visibility": "on"
+  }
+  ]
+  }
+  ]
+  });
+  map.addMarker({
+  position: mapLocation,
+  icon: 'images/icons/map-icon.png'
+  });
+  });
