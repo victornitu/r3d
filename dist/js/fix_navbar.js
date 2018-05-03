60ce8753135ba48ef49be5b96c35feb62198990f
Unknown <tilmansroger@gmail.com>
2018-05-03 18:02:43 -0300

    fixed navbar

---
+++ dist/js/fix_navbar.js
@@ -0,0 +1,21 @@
+function collapseNavbar() {
+  //console.log('hola');
+  //$(".navbar").removeClass(".bg-transparent");
+    //console.log($(".navbar").offset().top)
+    if ($(".navbar").offset().top > 50) {
+        console.log('1');
+        $(".navbar").removeClass("bg-transparent");
+        $(".navbar").addClass("not_transparent");
+      console.log('2');
+    } else {
+        $(".navbar").addClass("bg-transparent");
+        $(".navbar").removeClass(".not_transparent");
+    }
+}
+
+//function scrollAlert(collapseNavbar){
+  //alert('scroll');
+
+
+$(window).scroll(collapseNavbar);
+$(document).ready();
