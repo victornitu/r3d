e6908df2c95292bd89060b0178ebf4a81bfcec8b
Unknown <tilmansroger@gmail.com>
2018-05-11 12:49:31 -0300

    gtag fixed

--- dist/js/conversion_tag.js
+++ dist/js/conversion_tag.js
@@ -1,4 +1,1 @@
-<!-- Event snippet for contacto n conversion page -->
-<script>
-  gtag('event', 'conversion', {'send_to': 'AW-828605834/8VC2CMnKpYEBEIqLjosD'});
-</script>
+gtag('event', 'conversion', {'send_to': 'AW-828605834/8VC2CMnKpYEBEIqLjosD'});
--- dist/js/gtag.js
+++ dist/js/gtag.js
@@ -1,9 +1,5 @@
-<!-- Global Site Tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-82348627-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'UA-82348627-2');
-</script>
+gtag('config', 'UA-82348627-2');
